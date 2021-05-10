3c37a915a5a1ee97880d6f4ea5f49b59d3c0b252
fikri0511 <muhammadfikri.fadillah@gmail.com>
2021-05-11 06:30:14 +0700

    Initial Commit

---
+++ app/src/main/java/org/sonicboom/githubapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.sonicboom.githubapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sonicboom.githubapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
